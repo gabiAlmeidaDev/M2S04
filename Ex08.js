2455f1a48ef94cb9abd1ac971df3a359109c82fc
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-06-05 21:37:11 -0300

    Exercício 08

---
+++ Ex08.js
@@ -0,0 +1,44 @@
+class Produto {
+    constructor(nome, preco, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    vender(quantidadeVendida) {
+        if (quantidadeVendida > this.quantidade) {
+            console.log("Estoque insuficiente");
+        } else {
+            this.quantidade -= quantidadeVendida;
+            console.log(`${quantidadeVendida} unidades de ${this.nome} vendidas.`);
+        }
+    }
+
+    repor(quantidadeReposta) {
+        this.quantidade += quantidadeReposta;
+        console.log(`${quantidadeReposta} unidades de ${this.nome} foram repostas.`);
+    }
+
+    mostrarEstoque() {
+        console.log(`O produto ${this.nome.toUpperCase()} possui ${this.quantidade} unidades disponíveis.`);
+    }
+
+    atualizarPreco(novoPreco) {
+        this.preco = novoPreco;
+        console.log(`O preço do produto ${this.nome.toUpperCase()} foi atualizado para R$${this.preco.toFixed(2)}`);
+    }
+}
+
+const produto1 = new Produto("Notebook", 2500.00, 10);
+const produto2 = new Produto("Celular", 1500.00, 20);
+
+produto1.vender(5);  
+produto1.mostrarEstoque();  
+
+produto1.repor(10);  
+produto1.mostrarEstoque();  
+
+produto1.atualizarPreco(2600.00); 
+
+console.log(produto1);
+console.log(produto2);
